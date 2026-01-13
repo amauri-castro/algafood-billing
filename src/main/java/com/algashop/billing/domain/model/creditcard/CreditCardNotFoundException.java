309874c46e487b83e8ea65f2ec9acb0bcfccda69
Amauri Castro <amauri.p.castro@gmail.com>
2026-01-13 14:16:39 -0300

    implementado application service para geração de invoice

---
+++ src/main/java/com/algashop/billing/domain/model/creditcard/CreditCardNotFoundException.java
@@ -0,0 +1,6 @@
+package com.algashop.billing.domain.model.creditcard;
+
+import com.algashop.billing.domain.model.DomainException;
+
+public class CreditCardNotFoundException extends DomainException {
+}
